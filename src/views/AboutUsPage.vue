8edeb0e5eced19fd524d77791e767e4ec075138e
JhihKai0531 <jhihkai900531@outlook.com>
2025-01-27 01:50:06 +0800

    修改關於我們頁面的文案、調整樣式，並加入背景圖

--- src/views/AboutUsPage.vue
+++ src/views/AboutUsPage.vue
@@ -1,34 +1,61 @@
 <template>
-  <div class="container py-sm-5 py-4 px-3">
-    <h1 class="text-center pb-3">
-      <span class="">肉之家</span>
-      <span class="" style="color: rgb(207 160 13);">異國料理</span>
-    </h1>
-    <p>肉之家異國料理，無肉不歡者的聖地。這裡有破百種來自世界各地的肉類料理，舉凡雞、豬、牛、羊、海鮮，應有盡有。也有甜點、蔬食、濃湯等品項供應顧客享用。不管您是聚餐、約會、談生意，還是單獨前來，這裡都將提供給您美好的用餐體驗。</p>
-    <p>我們店內使用掃描 QR Code 點餐服務，您只需要動一動手指，瀏覽您喜歡的品項並勾選套餐及客製化選項，接著加入購物車。待所有品項確認無誤之後送出訂單，廚房將為您製作美味餐點。最後可以在訂單列表中瀏覽點餐的紀錄以及金額，吃飽後再進行結帳即可。</p>
-    <h2>營業資訊</h2>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">
-        營業時間：
-        <ul class="mt-2">
-          <li class="mt-1">每星期二~日</li>
-          <li class="mt-1">中午 11:00~14:30</li>
-          <li class="mt-1">晚上 17:00~21:00</li>
-        </ul>
-      </li>
-      <li class="list-group-item">餐廳地址：142 台北市中央區成功路一段100號</li>
-      <li class="list-group-item">連絡電話：02 0100 0010</li>
-    </ul>
+  <div class="outer">
+    <div class="container py-sm-5 py-4 px-3">
+      <h1 class="text-center mb-4">
+        肉之家異國料理
+      </h1>
+
+      <div class="mb-4">
+        <h3 class="fs-5 fw-bold">無肉不歡者的天堂，舌尖上的世界之旅！</h3>
+        <p>
+          來到肉之家，您將踏上一場美味的冒險之旅。我們精選全球破百道肉類料理，不論是多汁嫩雞、香濃豬排、頂級牛肉、鮮美羊排，還是新鮮海鮮，都能滿足您的挑剔味蕾。除此之外，還有讓人驚喜的甜點、健康蔬食和暖心濃湯，為每一位顧客提供多元選擇。
+        </p>
+        <p>無論是家庭聚會、浪漫約會、商務洽談，或是享受一場獨自的饗宴，肉之家都會讓您流連忘返。</p>
+      </div>
+
+      <div class="mb-4">
+        <h2 class="text-center mb-3">點餐方式</h2>
+        <h3 class="fs-5 fw-bold">輕鬆點餐，專屬美味即刻送上！</h3>
+        <p>
+          只需拿起手機掃描桌上的 QR Code，便能瀏覽我們的豐富菜單。選擇您心儀的餐點，客製化您的專屬風味，加入購物車後確認訂單即可。我們的廚師團隊將全力以赴，為您呈現每一道精心準備的美味料理。
+        </p>
+        <p>此外，您還可隨時查看訂單紀錄及消費金額，最後輕鬆結帳，享受無憂的用餐體驗。</p>
+      </div>
+
+      <h2 class="text-center">營業資訊</h2>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          營業時間：
+          <ul class="mt-2">
+            <li class="mt-1">週二至週日</li>
+            <li class="mt-1">午餐時段：11:00 ~ 14:30</li>
+            <li class="mt-1">晚餐時段：17:00 ~ 21:00</li>
+          </ul>
+        </li>
+        <li class="list-group-item">餐廳地址：142 台北市中央區成功路一段100號</li>
+        <li class="list-group-item">連絡電話：02-0100-0010</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
-h1 + p, h1 + p + p {
-  text-indent: 2em;
-  text-align: justify;
+/* 來源：https://unsplash.com/photos/woman-in-white-long-sleeve-shirt-holding-wine-glass-7BpuzmcxlHU */
+.outer {
+  /* background-image: url(@/assets/dinner.jpg); */
+  /* background-image: linear-gradient(rgb(255 255 255 / 0.3), rgb(255 255 255 / 0.3)), url(@/assets/dinner.jpg); */
+  background-image: linear-gradient(rgb(0 0 0 / 0.3), rgb(0 0 0 / 0.3)), url(@/assets/dinner.jpg);
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
-h2 {
-  margin-top: 1em;
+.container {
+  background-color: rgb(255 255 255 / 0.9);
+}
+
+.list-group-item {
+  background: inherit;
 }
 </style>
